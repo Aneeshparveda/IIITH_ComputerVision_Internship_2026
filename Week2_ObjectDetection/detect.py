c3730ec77459b4a0f80431590e48c4193fc3adbf
Aneesh Parveda <aneeshparveda@gmail.com>
2026-05-27 19:11:50 +0530

    Create detect.py

---
+++ Week2_ObjectDetection/detect.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Load pretrained YOLO model
+model = YOLO("yolov8n.pt")
+
+# Run object detection
+results = model(
+    "https://ultralytics.com/images/bus.jpg",
+    save=True
+)
+
+print("Object Detection Completed")
